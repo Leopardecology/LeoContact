2af8bb014d5c1ca387908bb92155968ea31ae560
phi7po@gmail.com <pipo@Philipps-MacBook-Pro.local>
2022-09-07 18:22:51 +0200

    fixed mongoose

--- server.js
+++ server.js
@@ -43,11 +43,10 @@ app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
 
 mongoose.connection.on('error', err => {
     console.log(err);
     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
 });
-
