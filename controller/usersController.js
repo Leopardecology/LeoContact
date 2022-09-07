b1bbda395eab186e6d165d109ed254d888336c28
phi7po@gmail.com <pipo@Philipps-MacBook-Pro.local>
2022-09-07 21:30:27 +0200

    changes note to contact

--- controller/usersController.js
+++ controller/usersController.js
@@ -11,7 +11,7 @@ const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find().select('-password').lean();
 
     // If no users 
-    if (!users) {
+    if (!users?.length) {
         return res.status(400).json({message: 'No users found'});
     }
     res.json(users);
--- models/User.js
+++ models/User.js
@@ -9,9 +9,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    role: [{
+    roles: [{
         type: String,
-        default: "User"
+        default: "Employee"
     }],
     active: {
         type: Boolean,
