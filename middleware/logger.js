c1f0c3b74280f41e11cd1df6ad159f4bb7d213cc
phi7po@gmail.com <pipo@Philipps-MacBook-Pro.local>
2022-09-07 16:42:25 +0200

    first middleware and logs

---
+++ middleware/logger.js
@@ -0,0 +1,27 @@
+const {format} = require('date-fns');
+const {v4: uuid} = require('uuid');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const logEvents = async (message, logFileName) => {
+    const dateTime = format(new Date(), 'dd-MM-yyyy\tHH:mm:ss');
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
+        }
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log'); //TODO: logs everything (change it)
+    console.log(`${req.method} ${req.path}`);
+    next();
+};
+
+module.exports = {logEvents, logger};
