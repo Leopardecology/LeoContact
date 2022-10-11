0267764814404943fe489f02e52213e41f7b6ea5
Sinrath <phi7po@gmail.com>
2022-10-11 11:37:23 +0200

    added auth tokens (not finished)

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -1,19 +1,63 @@
-import {Link} from 'react-router-dom';
+import {useEffect} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
+import {useNavigate, Link, useLocation} from 'react-router-dom';
+
+import {useSendLogoutMutation} from '../features/auth/authApiSlice';
+
+const DASH_REGEX = /^\/dash(\/)?$/;
+const CONTACTS_REGEX = /^\/dash\/contacts(\/)?$/;
+const USERS_REGEX = /^\/dash\/users(\/)?$/;
 
 const DashHeader = () => {
 
+    const navigate = useNavigate();
+    const {pathname} = useLocation();
+
+    const [sendLogout, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useSendLogoutMutation();
+
+    useEffect(() => {
+        if (isSuccess) navigate('/');
+    }, [isSuccess, navigate]);
+
+    if (isLoading) return <p>Logging Out...</p>;
+
+    if (isError) return <p>Error: {error.data?.message}</p>;
+
+    let dashClass = null;
+    if (!DASH_REGEX.test(pathname) && !CONTACTS_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
+        dashClass = "dash-header__container--small";
+    }
+
+    const logoutButton = (
+        <button
+            className="icon-button"
+            title="Logout"
+            onClick={sendLogout}
+        >
+            <FontAwesomeIcon icon={faRightFromBracket}/>
+        </button>
+    );
+
     const content = (
         <header className="dash-header">
-            <div className="dash-header__container">
+            <div className={`dash-header__container ${dashClass}`}>
                 <Link to="/dash">
                     <h1 className="dash-header__title">LeoContact</h1>
                 </Link>
                 <nav className="dash-header__nav">
-                    {/* add nav buttons later */}
+                    {/* add more buttons later */}
+                    {logoutButton}
                 </nav>
             </div>
         </header>
     );
+
     return content;
 };
 export default DashHeader;
