c29b19809d0eaa9de8f0dc8758bb4cd8b143b801
Sinrath <phi7po@gmail.com>
2022-10-16 18:59:28 +0200

    url typo

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {setCredentials} from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://172.104.1git40.140',
+    baseUrl: 'http://localhost:3500',
     credentials: 'include',
     prepareHeaders: (headers, {getState}) => {
         const token = getState().auth.token;
