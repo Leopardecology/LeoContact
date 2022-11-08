89ff35a075a5624a93d66a27acccf2dd381b2758
Sinrath <phi7po@gmail.com>
2022-11-08 16:57:51 +0100

    finished Login Modal closes #16

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -29,15 +29,9 @@ const Home = () => {
 
     const [login, {isLoading}] = useLoginMutation();
 
-    //TODO: Need to add a check to see if the user is already logged in ???
-    // useEffect(() => {
-    //     userRef.current.focus();
-    // }, []);
-
     useEffect(() => {
         setError('');
     }, [username, password]);
-
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -57,7 +51,6 @@ const Home = () => {
             } else {
                 setError(err.data?.message);
             }
-            // errRef.current.focus();
         }
     };
 
@@ -72,9 +65,6 @@ const Home = () => {
     return (
         <section className="public">
             <main className="home-page">
-
-                <p ref={errRef} className={errClass} aria-live="assertive">{errMsg}</p>
-
                 <Container>
                     <Row>
                         <Col><h1 className="main-title prevent-select">LeoContact</h1></Col>
@@ -92,10 +82,10 @@ const Home = () => {
                         keyboard={false}
                     >
                         <Modal.Header closeButton>
-                            <Modal.Title>Login</Modal.Title>
+                            <Modal.Title>Leo Contact</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <Form>
+                            <Form onSubmit={handleSubmit}>
                                 <Form.Group controlId="username"
                                             type="text"
                                             id="username"
@@ -103,49 +93,41 @@ const Home = () => {
                                             value={username}
                                             onChange={handleUserInput}
                                             autoComplete="off">
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter username"/>
-                                    <Form.Text className="text-muted">
-                                        We'll never share your email with anyone else.
-                                    </Form.Text>
+                                    <Form.Control type="text" placeholder="Username"/>
                                 </Form.Group>
+                            </Form>
 
+                            <Form onSubmit={handleSubmit}>
                                 <Form.Group controlId="password"
                                             type="password"
                                             id="password"
                                             value={password}
                                             onChange={handlePwdInput}>
-                                    <Form.Label>Password</Form.Label>
                                     <Form.Control type="password" placeholder="Password"/>
-                                    <Form.Text className="text-muted">
-                                        We'll never share your email with anyone else.
-                                    </Form.Text>
                                 </Form.Group>
                             </Form>
                         </Modal.Body>
                         <Modal.Footer>
+                            <Form.Group className="form-checkbox" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox"
+                                            label="Trust This Device"
+                                            id="persist"
+                                            onChange={handleToggle}
+                                            checked={persist}/>
+                            </Form.Group>
 
-                            <label htmlFor="persist" className="form__persist">
-                                <input
-                                    type="checkbox"
-                                    className="form__checkbox"
-                                    id="persist"
-                                    onChange={handleToggle}
-                                    checked={persist}
-                                />
-                                Trust This Device
-                            </label>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Close
-                            </Button>
-                            <Button variant="primary" onClick={handleSubmit}>LOGIN</Button>
+                            <p ref={errRef} className={errClass} aria-live="assertive">{errMsg}</p>
+
+                            <Button type="submit" variant="primary" onClick={handleSubmit}>LOGIN</Button>
+
                         </Modal.Footer>
+
+
                     </Modal>
                 </Container>
             </main>
         </section>
-    )
-        ;
+    );
 };
 
 export default Home;
